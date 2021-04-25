2f6cf7bb6090a96a57e3ea617ba91278f279003b
BurkClik <burk.clik@gmail.com>
2021-04-25 15:54:06 +0300

    Created sign up screen ui

--- app/src/main/java/school/cactus/succulentshop/IdentifierValidator.kt
+++ app/src/main/java/school/cactus/succulentshop/IdentifierValidator.kt
@@ -4,7 +4,7 @@ package school.cactus.succulentshop
 class IdentifierValidator : Validator {
     override fun validate(field: String) = when {
         field.isEmpty() -> R.string.this_field_is_required
-        field.length !in 4..51 -> R.string.identifier_is_too_short
+        field.length !in 4..50 -> R.string.identifier_is_too_short
         !field.contains('@') || !field.contains('.') -> R.string.email_is_invalid
         else -> null
     }
