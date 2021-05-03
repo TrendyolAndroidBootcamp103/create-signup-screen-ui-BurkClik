52576b41c653b672a304559da75351b18924117e
BurkClik <burk.clik@gmail.com>
2021-05-03 21:27:36 +0300

    Fix email length validator

--- app/src/main/java/school/cactus/succulentshop/signup/SignUpEmailValidator.kt
+++ app/src/main/java/school/cactus/succulentshop/signup/SignUpEmailValidator.kt
@@ -8,8 +8,9 @@ class SignUpEmailValidator : Validator {
     @StringRes
     override fun validate(field: String) = when {
         field.isEmpty() -> R.string.this_field_is_required
-        !field.contains('@') || !field.contains('.')
-                || field.length !in 6..50 -> R.string.email_is_invalid
+        !field.contains('@')
+                || !field.split('@')[1].contains('.')
+                || field.split('@')[0].length !in 6..50 -> R.string.email_is_invalid
         else -> null
     }
 }
